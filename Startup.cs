7dd1346ea8e58c13f9a0b81750d10f1444a7265c
Parthiban <uparthiban@hotmail.com>
2020-07-29 20:35:03 +0530

    commit 2

--- Startup.cs
+++ Startup.cs
@@ -38,10 +38,10 @@ namespace BankAPPWeb
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                //app.UseHsts();
             }
             app.UseSession();
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
